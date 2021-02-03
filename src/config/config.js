5b495afd754eb30e5e867a1dd113c2cea62e3d32
Titon <titondesign.kontakt@gmail.com>
2021-02-03 13:39:19 +0100

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config({
+    path: __dirname + "/.env"
+})
+    
+
+module.exports = {
+    prefix:"!",
+    programers: ["532605336926814229"], // Postępuj zgodnie z dokumentacją, aby uzupełnić config
+    testers: [ ],
+    supporters: [ ],
+}
